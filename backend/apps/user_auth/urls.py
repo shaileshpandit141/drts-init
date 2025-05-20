b0b810b999dc6e6d5969654905459be68ee90c78
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-05-20 23:06:19 +0530

    refactor: simplify user auth urls.

--- backend/apps/user_auth/urls.py
+++ backend/apps/user_auth/urls.py
@@ -19,32 +19,32 @@ from .views.verify_account_views import VerifyAccountConfirmView, VerifyAccountV
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
-    path("signin/token/", SigninTokenView.as_view(), name="signin_token"),
+    path("signin/token/", SigninTokenView.as_view(), name="signin-token"),
     path(
         "signin/token/refresh/",
         SigninTokenRefreshView.as_view(),
-        name="signin_token_refresh",
+        name="signin-token-refresh",
     ),
-    path("signout/", SignoutView.as_view(), name="sign_out"),
+    path("signout/", SignoutView.as_view(), name="signout"),
     path(
-        "verify-user-account/", VerifyAccountView.as_view(), name="verify_user_account"
+        "verify-account/", VerifyAccountView.as_view(), name="verify-account"
     ),
     path(
-        "verify-user-account/confirm/",
+        "verify-account/confirm/",
         VerifyAccountConfirmView.as_view(),
-        name="verify_user_account_confirm",
+        name="verify-account-confirm",
     ),
-    path("change-password/", ChangePasswordView.as_view(), name="change_password"),
-    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot_password"),
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+    path("forgot-password/", ForgotPasswordView.as_view(), name="forgot-password"),
     path(
         "forgot-password/confirm/",
         ForgotPasswordConfirmView.as_view(),
-        name="forgot_password_confirm",
+        name="forgot-password-confirm",
     ),
     path(
         "deactivate-account/",
         DeactivateAccountView.as_view(),
-        name="deactivate_account",
+        name="deactivate-account",
     ),
     path("user/", UserInfoView.as_view(), name="user-info"),
 ]
