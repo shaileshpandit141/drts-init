e398d6c452398c35d7af5b69835feeb644cd756e
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-14 16:57:04 +0530

    feat: define custom exception for load environment variabls

---
+++ backend/exceptions/__init__.py
@@ -0,0 +1,3 @@
+from .missing_environment_variable_error import MissingEnvironmentVariableError
+
+__all__ = ["MissingEnvironmentVariableError"]
---
+++ backend/exceptions/missing_environment_variable_error.py
@@ -0,0 +1,2 @@
+class MissingEnvironmentVariableError(Exception):
+    """Raised when a required environment variable is missing."""
