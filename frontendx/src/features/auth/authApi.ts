12d04a62de8fbc6bc6fe5df26a887f26588757a5
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-25 18:52:05 +0530

    chore: seprate redux base query

--- frontendx/src/features/auth/authApi.ts
+++ frontendx/src/features/auth/authApi.ts
@@ -1,25 +1,11 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type {
-  SigninRequest,
-  SigninResponse,
-} from "./types";
+import { createApi } from "@reduxjs/toolkit/query/react";
+import type { SigninRequest, SigninResponse } from "./types";
 import { setCredentials, signout } from "./authSlice";
-import type { RootState } from "../../app/store";
-
-const BASE_URL = "http://localhost:8000/api/v1";
+import { baseQueryAuth } from "../../app/baseQueryAuth";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.access_token;
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryAuth,
   endpoints: (builder) => ({
     signin: builder.mutation<SigninResponse, SigninRequest>({
       query: (credentials) => ({
