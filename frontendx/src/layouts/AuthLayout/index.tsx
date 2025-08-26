ed9afc8aa7b70dadcd30fd42e8e950e9d97dcf61
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-26 11:56:37 +0530

    feat: create auth and main layout

---
+++ frontendx/src/layouts/AuthLayout/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC, JSX } from "react";
+import "./AuthLayout.module.css";
+
+const AuthLayout: FC = (): JSX.Element => {
+    return (
+        <div>AuthLayout</div>
+    )
+}
+
+export default AuthLayout;
---
+++ frontendx/src/layouts/MainLayout/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC, JSX } from "react";
+import "./MainLayout.module.css";
+
+const MainLayout: FC = (): JSX.Element => {
+    return (
+        <div>MainLayout</div>
+    )
+}
+
+export default MainLayout;
