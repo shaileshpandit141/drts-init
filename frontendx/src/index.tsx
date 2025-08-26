485d336f4c4b06d2da99411009620077ea2f2f29
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-08-26 11:26:22 +0530

    style: add root styles

--- frontendx/src/index.tsx
+++ frontendx/src/index.tsx
@@ -4,6 +4,7 @@ import "./styles/theme.css";
 import "./styles/variables.css";
 import "./styles/reset.css";
 import "./styles/typography.css";
+import "./styles/root.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from './App';
