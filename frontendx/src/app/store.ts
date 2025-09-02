7c05780d141f16c58534ebc8adc6024ac8d4b906
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-09-02 15:57:05 +0530

    feat: register signout feature to store

--- frontendx/src/app/store.ts
+++ frontendx/src/app/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "features/auth/authSlice";
+import toastReducer from "features/toast/toastSlice";
 import { authApi } from "features/auth/authApi";
-import { toastReducer } from "features/toast";
+import authReducer from "features/auth/authSlice";
+import { signoutApi } from "features/signout/signoutApi";
+import signoutReducer from "features/signout/signoutSlice";
 
 export const store = configureStore({
   reducer: {
+    toast: toastReducer,
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
-    toast: toastReducer,
+    signout: signoutReducer,
+    [signoutApi.reducerPath]: signoutApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, signoutApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
