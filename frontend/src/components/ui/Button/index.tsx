e505e40c8b3cdf40d96fea3d68a3e541951141bf
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-09-07 21:30:29 +0530

    feat: add custom button component

---
+++ frontend/src/components/ui/Button/index.tsx
@@ -0,0 +1,47 @@
+import React, { FC, JSX } from "react";
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+    children?: string | JSX.Element;
+    icon?: {
+        left?: JSX.Element,
+        right?: JSX.Element
+    };
+    title?: string;
+    className?: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+    disabled?: boolean
+
+}
+
+const Button: FC<ButtonProps> = (props): JSX.Element | null => {
+    const { children, icon, title, className, onClick, disabled = false } = props;
+
+    const setTitle = () => {
+        if (!title && typeof children === "string") {
+            return children;
+        }
+        return title
+    }
+
+    return (
+        <button
+            className={`${styles.button} ${className}`}
+            title={setTitle()}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            <span className={styles.icon}>
+                {icon && icon.left}
+            </span>
+            {children && (
+                <span className={styles.text}>{children}</span>
+            )}
+            <span className={styles.icon}>
+                {icon && icon.right}
+            </span>
+        </button>
+    )
+}
+
+export default Button;
