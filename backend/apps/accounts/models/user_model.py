6e62554842b8a3e8f44cdbf8754be829989c1d2a
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-09-16 19:44:46 +0530

    feat: update mata class overwride in user model

--- backend/apps/accounts/models/user_model.py
+++ backend/apps/accounts/models/user_model.py
@@ -26,7 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     and PermissionsMixin.
     """
 
-    class Meta(AbstractBaseUser.Meta, PermissionsMixin.Meta):  # type: ignore  # noqa: PGH003
+    class Meta():
         db_table = "users"
         verbose_name = "user"
         verbose_name_plural = "users"
